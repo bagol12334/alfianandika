5e95e2b0641f56089cdc2be356ae56573f43edb4
bagol12334 <101828171+bagol12334@users.noreply.github.com>
2022-03-18 01:29:06 +0700

    Create bagol.sh

---
+++ .github/bagol.sh
@@ -0,0 +1,8 @@
+! wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip 
+! unzip ngrok-stable-linux-amd64.zip
+! ./ngrok authtoken masukantokenngrok
+! wget https://raw.githubusercontent.com/admin123356/bagol1/main/CRD_COLAB_EXE.sh &> /dev/null
+! chmod +x CRD_COLAB_EXE.sh
+! ./CRD_COLAB_EXE.sh
+
+
